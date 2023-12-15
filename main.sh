91c19513b64e011a76e61093249f3cdec1fa8d14
Arthur Lorenzon <arthurgabriellp@gmail.com>
2023-12-14 21:04:27 -0300

    Atualizado main.sh para executar arthur-lorenzon.sh e verificar se uma pasta foi criada

--- main.sh
+++ main.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Nome da Equipe: Arthur Lorenzon"
+
+bash arthur-lorenzon.sh
+
+if [ -d "arthur-lorenzon" ]; then
+	echo "A apasta 'arthur-lorenzon está disponível."
+else 
+	echo "A pasta 'arthur-lorenzon' não está disponível."
+fi
+
